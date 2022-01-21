da4c97e577639ef268568c40cf8adc1f539576c4
scottrohrig <scott.rohrig@gmail.com>
2022-01-21 12:41:22 -0800

    add dotenv for mysql2 creds

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,13 @@
 const mysql = require( 'mysql2' );
+require('dotenv').config();
+
+const {MYSQL_UN, MYSQL_PW} = process.env;
 
 // Connect to database
 const db = mysql.createConnection( {
     host: 'localhost',
-    user: 'root',
-    password: '',
+    user: MYSQL_UN,
+    password: MYSQL_PW,
     database: 'election'
 } );
 
